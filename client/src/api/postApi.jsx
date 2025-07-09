c0f26a5e62d2c1d811f3879bec51ee99e8691027
Bhavya-Gupta-1811 <bhavya042btcse23@igdtuw.ac.in>
2025-07-09 17:19:36 +0530

    Update postApi.jsx

--- client/src/api/postApi.jsx
+++ client/src/api/postApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: "https://disease-predictor-1-2.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
