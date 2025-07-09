6f6223a56e80bc4320eb5fadb62e45d358a2d736
Bhavya-Gupta-1811 <bhavya042btcse23@igdtuw.ac.in>
2025-07-09 17:28:16 +0530

    Update auth.jsx

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -23,7 +23,7 @@ export const AuthProvider = ({ children }) => {
 
   const userAuthentication = async () => {
     try {
-      const response = await fetch("https://disease-predictor-1-a2y1.onrender.com", {
+      const response = await fetch("https://disease-predictor-1-a2y1.onrender.com/api/auth/users", {
         method: "GET",
         headers: {
           Authorization:`Bearer ${token}`,
